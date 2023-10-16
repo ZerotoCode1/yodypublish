6156df727d7593ac14763ecdfddde14631a3e3e4
ZerotoCode1 <quang11102002f@gmail.com>
2023-10-16 08:56:44 +0700

    public

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,82 @@
+import { useEffect, useReducer, useState ,useRef} from "react";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../components/Product";
+import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import io from "socket.io-client";
+const socket = io.connect("https://socketio-rhix.onrender.com");
+// import data from '../data';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+
+
+function HomeScreen() {
+  const [productss,setProducts]  =useState()
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  // const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+     
+        socket.emit('getproduct')
+        socket.on('productData',function(result){
+            const data1=result
+            setProducts(result)
+          dispatch({ type: "FETCH_SUCCESS", payload: data1.data });
+        })
+        //const result = await axios.get("https://apiwenandapp.onrender.com/api/products");
+       
+       
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+
+      // setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <Helmet>
+        <title>Amazona</title>
+      </Helmet>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <LoadingBox />
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <Row>
+            {productss.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
+    </div>
+  );
+}
+export default HomeScreen;
