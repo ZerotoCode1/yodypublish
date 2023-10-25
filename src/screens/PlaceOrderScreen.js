5f2bc5d7940039314e9cf56e77fcc40639758b45
ZerotoCode1 <quang11102002f@gmail.com>
2023-10-25 12:59:51 +0700

    send mail

--- src/screens/PlaceOrderScreen.js
+++ src/screens/PlaceOrderScreen.js
@@ -49,7 +49,7 @@ export default function PlaceOrderScreen() {
       dispatch({ type: 'CREATE_REQUEST' });
 
       const { data } = await Axios.post(
-        '/api/orders',
+        'https://apiwenandapp.onrender.com/api/orders',
         {
           orderItems: cart.cartItems,
           shippingAddress: cart.shippingAddress,
